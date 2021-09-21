210048a31a1e9144425cf63eca80a2260209b693
wbrown97a <60943598+wbrown97a@users.noreply.github.com>
2021-09-21 10:21:31 -0400

    Update quadratic_solver.py

--- quadratic_solver.py
+++ quadratic_solver.py
@@ -4,7 +4,7 @@
 
 #9.121.2021
 from math import sqrt
-
+#Created function to tell me the roots when I call it
 def discriminate():
     dis = b * b - 4 * a * c 
     if dis > 0: 
@@ -22,16 +22,16 @@ def discriminate():
 a = int(input("Enter coeeficient a: "))
 b = int(input("Enter coeeficient b: "))
 c = int(input("Enter coeeficient c: "))
-discriminate()
+discriminate() #function gets called
 
 
-
+#loops code so the user can press enter and run it again without restarting the program
 x = input("Press x to quit Press enter to run again")
 while x == (""):
     a = int(input("Enter coeeficient a: "))
     b = int(input("Enter coeeficient b: "))
     c = int(input("Enter coeeficient c: "))
-    discriminate()
+    discriminate() #function gets called
 
     
     x = input("Press x to quit Press enter to run again")
