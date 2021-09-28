12f606e8a0450603a6e641ee7efdebc290257696
wbrown97a <60943598+wbrown97a@users.noreply.github.com>
2021-09-28 10:24:19 -0400

    Create msp.py

---
+++ msp.py
@@ -0,0 +1,18 @@
+# Python Program Challenge - MSP (ENGR4)
+
+#Written by Watts Brown
+
+#9.28.2021
+
+
+
+word = input("Player 1 Enter Word:")
+guess = input("Guess a letter: ")
+letters = word.split()
+turns = 7
+print("|---┐")
+
+while (turns > 0) :
+
+    print("|---┐")
+   
