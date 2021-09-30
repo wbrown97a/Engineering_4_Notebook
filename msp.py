e984ff08605906fe93e6394e795c2f5caef177fa
wbrown97a <60943598+wbrown97a@users.noreply.github.com>
2021-09-30 10:14:21 -0400

    Update msp.py

--- msp.py
+++ msp.py
@@ -4,15 +4,51 @@
 
 #9.28.2021
 
+head =    "   0"
+twoArms = "  \|/"
+torso =   "   |"
+twoLegs = "  / \ "
 
+def body(limbs):
+    print("|---┐")  
+    if limbs == 4:
+        print("")
+        print("")
+        print("")
+        print("")
+    if limbs == 3:
+        print(head)
+        print("")
+        print("")
+        print("")
+    if limbs == 2:
+        print(head)
+        print(twoArms)
+        print("")
+        print("")
+    if limbs == 1:
+        print(head)
+        print(twoArms)
+        print(torso)
+        print("")
+    if limbs == 0:
+        print(head)
+        print(twoArms)
+        print(torso)
+        print(twoLegs)
+        
+def wordLength():
+    length = len(word)
+    print ("_ " * length)
 
-word = input("Player 1 Enter Word:")
-guess = input("Guess a letter: ")
-letters = word.split()
-turns = 7
-print("|---┐")
+word = input("Player 1 Enter a word")
+limbs = 4
+guesses = 0 
+print("\n" *50 )
+wordLength()
+guess = input("Player 2 Guess a letter")
+letter = list(word)
+body(limbs)
 
-while (turns > 0) :
-
-    print("|---┐")
-   
+while (limbs > 0 and guess != word) :
+    failed = 0 
